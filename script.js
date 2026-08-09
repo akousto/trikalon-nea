e761ccf7f56fca9d93db3e49a21c2daf4c79e918
akousto <info3gel@gmail.com>
2026-08-09 22:23:11 +0300

    Add audio play event to pause other audio elements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+document.querySelectorAll("audio").forEach(audio => {
+    audio.addEventListener("play", function () {
+        document.querySelectorAll("audio").forEach(other => {
+            if (other !== this) {
+                other.pause();
+                other.currentTime = 0;
+            }
+        });
+    });
+});
