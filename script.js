95bef1498452673f5ac991cf80cf02cd2b8d2fb2
akousto <info3gel@gmail.com>
2026-08-10 10:19:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+// ---------------------------------------------
+// Όταν πατώ play τότε stop σε όλα τα άλα mp3
 document.querySelectorAll("audio").forEach(audio => {
     audio.addEventListener("play", function () {
         document.querySelectorAll("audio").forEach(other => {
@@ -8,4 +9,30 @@ document.querySelectorAll("audio").forEach(audio => {
             }
         });
     });
+});
+
+// ---------------------------------------------
+// Μέγεθος γραμματοσειράς
+
+const increaseButton = document.getElementById("increaseFont");
+const decreaseButton = document.getElementById("decreaseFont");
+
+let fontSize = localStorage.getItem("fontSize");
+
+if (fontSize === null) {
+fontSize = 22;
+}
+
+document.body.style.fontSize = fontSize + "px";
+
+increaseButton.addEventListener("click", function () {
+fontSize = Number(fontSize) + 2;
+document.body.style.fontSize = fontSize + "px";
+localStorage.setItem("fontSize", fontSize);
+});
+
+decreaseButton.addEventListener("click", function () {
+fontSize = Number(fontSize) - 2;
+document.body.style.fontSize = fontSize + "px";
+localStorage.setItem("fontSize", fontSize);
 });
